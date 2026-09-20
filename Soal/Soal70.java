14f0d6e452902d7ba2703a9ffba5a6a0610216e9
Nadya <miss.nads7@gmail.com>
2026-09-21 06:06:47 +0700

    Rapikan file kedalam soal

---
+++ Soal/Soal70.java
@@ -0,0 +1,11 @@
+public class Soal70 {
+    public static void main(String[] args) {
+
+        System.out.println("0000000");
+        System.out.println("*******");
+        System.out.println("=======");
+        System.out.println("0000000");
+        System.out.println("*******");
+        System.out.println("=======");
+    }
+}
